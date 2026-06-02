629c2f2c41458728d8eb37a747984e65af9e2846
areo26 <himanshusaini2705@gmail.com>
2026-06-02 08:47:03 +0530

    Added BFS & DFS Algorithm

---
+++ Algorithms/Graps/bfs_dfs.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <stack>
+
+std::vector<int> bfs(const std::vector<std::vector<int>>& adjMat, int startNode) {
+    int size = adjMat.size();
+    std::queue<int> q;
+    std::vector<bool> visited (size, false);
+    std::vector<int> traverse;
+    q.push(startNode);
+    visited[startNode] = true;
+
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        traverse.push_back(node);
+        for(int i = 0; i < size; i++) {
+            if(adjMat[node][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+    return traverse;
+}
+
+std::vector<int> dfs(const std::vector<std::vector<int>>& adjMat, int startNode) {
+    int size = adjMat.size();
+    std::stack<int> s;
+    std::vector<bool> visited (size, false);
+    std::vector<int> traverse;
+    s.push(startNode);
+    while(!s.empty()) {
+        int node = s.top();
+        s.pop();
+        if(visited[node]) continue;
+        visited[node] = true;
+        traverse.push_back(node);
+        for(int i = 0; i < size; i++) {
+            if(adjMat[node][i] == 1 && !visited[i]) {
+                s.push(i);
+            }
+        }
+    }
+    return traverse;
+}
+
+int main() {
+    std::vector<std::vector<int>> adjMat = {
+        {0,1,1,0,0,0},
+        {1,0,0,1,0,0},
+        {1,0,0,1,0,0},
+        {0,1,1,0,1,1},
+        {0,0,0,1,0,1},
+        {0,0,0,1,1,0}
+    };
+    std::vector<int> dfs_t = dfs(adjMat, 0);
+    for(int i : dfs_t) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    std::vector<int> bfs_t = bfs(adjMat, 0);
+    for(int i : bfs_t) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
